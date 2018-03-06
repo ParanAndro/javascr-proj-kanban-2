ccff5d011659111a5d6c753d90fb6aedefe6c8a5
Radek Izdebski <radek1izdebski@gmail.com>
2018-03-06 18:17:12 +0100

    Changed js files

---
+++ js/card.js
@@ -0,0 +1,36 @@
+// KLASA KANBAN CARD
+function Card(id, name) {
+	var self = this;
+	
+	this.id = id;
+	this.name = name || 'No name given';
+	this.element = createCard();
+
+	function createCard() {
+		var card = $('<li>').addClass('card');
+		var cardDeleteBtn = $('<button>').addClass('btn-delete').text('x');
+		var cardDescription = $('<p>').addClass('card-description').text(self.description);
+		
+		cardDeleteBtn.click(function(){
+			self.removeCard();
+		});
+		
+		card.append(cardDeleteBtn);
+		cardDescription.text(self.name);
+		card.append(cardDescription);
+		console.log(card);
+		return card;
+	}
+}
+Card.prototype = {
+	removeCard: function() {
+		var self = this;
+		$.ajax({
+			url: baseUrl + '/card/' + self.id,
+			method: 'DELETE',
+			success: function(){
+			self.$element.remove();
+			}
+		});
+	}
+}
